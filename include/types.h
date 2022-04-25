055c772d583b157824b8cc850e9b95c190738924
Vince R <r2d2fan20@outlook.com>
2022-04-24 23:54:51 -0500

    types

---
+++ include/types.h
@@ -0,0 +1,26 @@
+//
+// Created by Vince on 4/24/22.
+//
+
+#ifndef SHADOW_NATIVE_TYPES_H
+#define SHADOW_NATIVE_TYPES_H
+
+#include <cinttypes>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef std::size_t usize;
+typedef __ssize_t isize;
+
+typedef float f32;
+typedef double f64;
+
+#endif //SHADOW_NATIVE_TYPES_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include "main.h"
 #include "DebugUI.h"
+#include "types.h"
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
 
 #include <bgfx/bgfx.h>
 #include <bx/bx.h>
@@ -58,6 +63,39 @@ static void toggleStats() {
 static void toggleDebugger() {
     s_showDebugger = !s_showDebugger;
 }
+
+static const glm::vec2 SIZE = glm::vec2(1280, 720);
+
+struct Vertex {
+    glm::vec3 position;
+    u32 color;
+};
+
+static Vertex cube_vertices[] = {
+        { glm::vec3(-1.0f,  1.0f,  1.0f), 0xff000000 },
+        { glm::vec3( 1.0f,  1.0f,  1.0f), 0xff0000ff },
+        { glm::vec3(-1.0f, -1.0f,  1.0f), 0xff00ff00 },
+        { glm::vec3( 1.0f, -1.0f,  1.0f), 0xff00ffff },
+        { glm::vec3(-1.0f,  1.0f, -1.0f), 0xffff0000 },
+        { glm::vec3( 1.0f, 1.0f, -1.0f), 0xffff00ff },
+        { glm::vec3(-1.0f, -1.0f, -1.0f), 0xffffff00 },
+        { glm::vec3( 1.0f, -1.0f, -1.0f), 0xffffffff },
+};
+
+static const u16 cube_indices[] = {
+        0, 1, 2,
+        1, 3, 2,
+        4, 6, 5,
+        5, 6, 7,
+        0, 2, 4,
+        4, 2, 6,
+        1, 5, 3,
+        5, 7, 3,
+        0, 4, 1,
+        4, 5, 1,
+        2, 3, 6,
+        6, 3, 7,
+};
 
 int main() {
 
