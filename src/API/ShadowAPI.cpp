703bce721a6930a3c5c84b6096678f65283e2595
Vince R <46038462+77Z@users.noreply.github.com>
2023-08-25 22:31:27 -0500

    Attempt to patch create the API

--- src/API/ShadowAPI.cpp
+++ src/API/ShadowAPI.cpp
@@ -1,6 +1,8 @@
 // API meant to be called by UserCode and NOT internal code
 
-#include "API/ShadowAPI.h"
+#include "API/ShadowAPI.hpp"
+#include "Debug/Logger.hpp"
+#include <string>
 
 bool isShadowEngineDebugMode() {
 #ifdef SHADOW_DEBUG_BUILD
@@ -9,3 +11,5 @@ bool isShadowEngineDebugMode() {
 	return false;
 #endif
 }
+
+void simplePrint(std::string input) { PRINT("%s", input.c_str()); }
