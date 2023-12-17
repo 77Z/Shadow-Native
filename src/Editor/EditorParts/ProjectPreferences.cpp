1080d1b2e54ef933cc266699ea021b667a0fce28
vince <r2d2fan20@outlook.com>
2023-12-17 15:28:32 -0600

    Stuff and trhings

--- src/Editor/EditorParts/ProjectPreferences.cpp
+++ src/Editor/EditorParts/ProjectPreferences.cpp
@@ -1,4 +1,6 @@
+#include "Editor/Project.hpp"
 #include "imgui.h"
+#include "json_impl.hpp"
 
 namespace Shadow::Editor::EditorParts {
 
@@ -14,7 +16,13 @@ void projectPreferencesUpdate() {
 
 	ImGui::Begin("Project Preferences", &shouldDrawProjectPreferences);
 	
-	ImGui::Text("WHhhhhaatt");
+	ImGui::Text("%s", Editor::getCurrentProjectPath().c_str());
+
+	json projectSec = JSON::readBsonFile(Editor::getCurrentProjectPath() + "/project.sec");
+
+	ImGui::Text("Package ID: %s", ((std::string)projectSec["pkgId"]).c_str());
+	ImGui::Text("Package ID: %s", ((std::string)projectSec["pkgId"]).c_str());
+	ImGui::Text("Build for Shadow Engine version: %s", ((std::string)projectSec["engineVer"]).c_str());
 
 	ImGui::End();
 
