93451cb2ec34b99eda844d2660644674e4d8634e
Vince R <vince@77z.dev>
2025-09-14 22:52:32 -0500

    working towards VSTs (editor not pushed?)

--- src/AXE/AxevstPlugins.cpp
+++ src/AXE/AxevstPlugins.cpp
@@ -4,13 +4,24 @@
 
 #include "AxevstPlugins.hpp"
 
+#include "Debug/EditorConsole.hpp"
+#include "bx/bx.h"
 #include "imgui.h"
+#include "json_impl.hpp"
+
+#include <filesystem>
+
+#define EC_THIS "AXE VST"
 
 namespace Shadow::AXE {
 
-AXEVSTPlugins::AXEVSTPlugins() {  }
+AXEVSTPlugins::AXEVSTPlugins() {
+	EC_NEWCAT(EC_THIS);
 
-AXEVSTPlugins::~AXEVSTPlugins() {  }
+	reloadVSTPlugins();
+}
+
+AXEVSTPlugins::~AXEVSTPlugins() = default;
 
 void AXEVSTPlugins::onUpdate(bool& p_open) {
 	using namespace ImGui;
@@ -23,9 +34,68 @@ void AXEVSTPlugins::onUpdate(bool& p_open) {
 	}
 
 	SeparatorText("Available Plugins:");
+	SameLine();
+	if (SmallButton("re-scan")) reloadVSTPlugins();
+
+	for (auto& vst : indexedVsts) {
+		Selectable(vst.prettyName.c_str());
+		SetItemTooltip("%s - %s", vst.version.c_str(), vst.vendor.c_str());
+	}
 
 	End();
 }
+
+std::filesystem::path AXEVSTPlugins::getVSTStorageLocation() {
+	std::string path;
+
+#ifdef BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+	path = static_cast<std::string>(getenv("HOME")) + "/.vst3/";
+#elif BX_PLATFORM_WINDOWS
+	path = "C:\\Program Files\\Common Files\\VST3";
+#else
+#	error "COMPILATION ERROR: AXE DOESN'T KNOW WHERE VSTs GO ON THIS PLATFORM"
+#endif
+
+	return std::filesystem::path(path);
+}
+
+void AXEVSTPlugins::reloadVSTPlugins() {
+	EC_PRINT(EC_THIS, "Discovering plugins:");
+	for (auto& vstDir : std::filesystem::directory_iterator(getVSTStorageLocation())) {
+		BasicVstEntry entry;
+		EC_PRINT(EC_THIS, "- %s", vstDir.path().filename().c_str());
+
+		// Validate that the shared lib file exists (probably should have a more extensive check.
+		// e.g. loading it, grabbing plugin name)
+		// std::filesystem::exists(vstDir.path() / "Contents" / "");
+
+		entry.name = vstDir.path().filename().c_str();
+		entry.path = vstDir.path();
+
+
+		// Is there a moduleinfo.json we can read?
+		std::filesystem::path moduleInfoPath = vstDir.path() / "Contents" / "Resources" / "moduleinfo.json";
+		if (std::filesystem::exists(moduleInfoPath)) {
+			try {
+				auto moduleInfoData = JSON::readJsonFile(moduleInfoPath);
+
+				entry.prettyName = moduleInfoData["Name"];
+				entry.version = moduleInfoData["Version"];
+				entry.vendor = moduleInfoData["Factory Info"]["Vendor"];
+				entry.url = moduleInfoData["Factory Info"]["URL"];
+				entry.email = moduleInfoData["Factory Info"]["E-Mail"];
+			} catch (const std::exception& e) {
+				entry.prettyName = entry.name;
+			}
+		} else {
+			entry.prettyName = entry.name;
+		}
+
+		indexedVsts.emplace_back(entry);
+	}
+	EC_PRINT(EC_THIS, "-----------------------------------");
+}
+
 
 
 }
--- src/AXE/AxevstPlugins.hpp
+++ src/AXE/AxevstPlugins.hpp
@@ -5,7 +5,21 @@
 #ifndef SHADOW_NATIVE_AXEVSTPLUGINS_HPP
 #define SHADOW_NATIVE_AXEVSTPLUGINS_HPP
 
+#include <filesystem>
+#include <string>
+#include <vector>
+
 namespace Shadow::AXE {
+
+	struct BasicVstEntry {
+		std::string prettyName;
+		std::string name;
+		std::string vendor;
+		std::string version;
+		std::string url;
+		std::string email;
+		std::filesystem::path path;
+	};
 
 	class AXEVSTPlugins {
 	public:
@@ -13,6 +27,14 @@ namespace Shadow::AXE {
 		~AXEVSTPlugins();
 
 		void onUpdate(bool& p_open);
+
+		static std::filesystem::path getVSTStorageLocation();
+
+		/// Scans VST path to index plugins
+		void reloadVSTPlugins();
+
+	private:
+		std::vector<BasicVstEntry> indexedVsts;
 	};
 
 } // Shadow::AXE
